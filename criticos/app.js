6b7fb977ceb76413c081415873e0175b1d252b49
Rafael Andrade <rafaelreis2011@gmail.com>
2024-12-19 10:33:40 -0500

    update img & css

--- criticos/app.js
+++ criticos/app.js
@@ -9,3 +9,12 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const navbarToggler = document.querySelector(".navbar-toggler");
+    const navbarCollapse = document.querySelector("#navbarNav");
+
+    navbarToggler.addEventListener("click", () => {
+        const expanded = navbarToggler.getAttribute("aria-expanded") === "true";
+        navbarToggler.setAttribute("aria-expanded", !expanded);
+    });
+});
